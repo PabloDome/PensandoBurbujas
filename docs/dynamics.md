2586432f6119400c0c6da52277e46da2a4396d11
sebastlop <sebastlop@gmail.com>
2024-06-06 15:43:35 -0300

    particles travel very nice

---
+++ docs/dynamics.md
@@ -0,0 +1,28 @@
+# Dynamic of the particle system
+
+Here we explore the dynamics of the particle system. For the particle system with masses ${m_i}$ and coordinates ${\mathbf{r}_i}$, we have the coupled system of differential equations:
+$$
+\begin{equation}
+    m_i \ddot{\mathbf{r}}_i = -k \left(2\mathbf{r}_i-\mathbf{r}_{i-1} -\mathbf{r}_{i+1} \right),
+\end{equation}
+$$
+with cyclic conditions.
+
+To solve the former system, we will use numerical algorithms, particularly one of the so-called _Leap-frog_ schemes: the kick-drift-kick_. This integration method propose the following scheme:
+
+* The second-order $n$ equations are is splitted into $2n$ first-order differential equations
+$$
+\begin{align}
+    \dot{\mathbf{v}}_i &= &\mathbf{a_i}, \\
+    \dot{\mathbf{r}}_i &= &\mathbf{v_i},
+\end{align}
+$$
+
+* Given the accelerations $\mathbf{a_i}=\mathbf{F_i}/m_i$, velocities $\mathbf{v_i}$ and positions $\mathbf{r}_i $ at the instant $t_i$, we can calculate the next step as follows:
+$$
+\begin{align}
+v_{i+1/2} & = v_{i}+a_{i}{\frac {\Delta t}{2}},\\
+x_{i+1}   & = x_{i}+v_{i+1/2}\Delta t,\\
+v_{i+1}   & = v_{i+1/2}+a_{i+1}{\frac {\Delta t}{2}},
+\end{align}
+$$
--- src/dynamics.py
+++ src/dynamics.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def get_forces_zero_rest_distance(x, y, k):
+def get_accel_zero_rest_distance(r, k_over_m):
     '''
     this function calculate the forces for each particle
     considering constrains as springs. All springs are identical
@@ -11,26 +11,49 @@ def get_forces_zero_rest_distance(x, y, k):
     '''
     # we put the last coordinates at the beginning and first at the end
     # cyclic conditions
-    new_x = np.insert(x,0,x[-1])
-    new_x = np.append(new_x,x[0])
-    new_y = np.insert(y,0,y[-1])
-    new_y = np.append(new_y,y[0])
-
+    new_r = np.hstack([r[:,-1].reshape(2,-1),r,r[:,0].reshape(2,-1)])
     # Calculate the forces
-    n = len(x)
-    forces_x = np.zeros(n)
-    forces_y = np.zeros(n)
-    
-    forces_x = -k*(2*x-new_x[:-2]-new_x[2:])
-    forces_y = -k*(2*y-new_y[:-2]-new_y[2:]) 
+    accel = -k_over_m*(2*r-new_r[:,:-2]-new_r[:,2:])
+    return accel
 
+def plot_particles_and_forces(r,f):
+    plt.figure(figsize=(6,6))
+    plt.scatter(r[0,:],r[1,:])
+    plt.quiver(r[0,:],r[1,:],f[0,:],f[1,:])
+    plt.show()
 
-    return forces_x, forces_y
+def kick_drift_kick_step(r, v, k, dt):
+    a_old  = get_accel_zero_rest_distance(r, k)
+    v_half = v + a_old * dt/2
+    r_new  = r + v_half * dt
+    a_new  = get_accel_zero_rest_distance(r, k)
+    v_new  = v_half + a_new * dt/2
+    return r_new, v_new
 
 if __name__ == '__main__':
     from geometry import get_poligon
-    x,y = get_poligon()
-    fx, fy = get_forces_zero_rest_distance(x,y,0.1)
-    plt.scatter(x,y)
-    plt.quiver(x,y,fx,fy)
+    import matplotlib.animation as animation
+    x,y = get_poligon(n = 120, R0=1.)
+    r = np.vstack([x,y])
+    a = get_accel_zero_rest_distance(r, 0.1)
+    plot_particles_and_forces(r,a)
+    v = np.zeros_like(r)
+    plt.figure(figsize=(6,6))
+    plt.title('time evolution')
+    r_med = []
+    v_med = []
+    for i in range(1000):
+        r, v = kick_drift_kick_step(r,v,0.1,0.1)
+        v_med.append(np.sqrt(np.apply_along_axis(lambda x: x**2,0, v)).mean())
+        r_med.append(np.sqrt(np.apply_along_axis(lambda x: x**2,0, r)).mean())
+        if i%200 == 0:
+            plt.scatter(r[0,:], r[1,:], label=f'timestep: {i}')
+            plt.quiver(r[0,:], r[1,:],v[0,:], v[1,:])
+    plt.legend()
+    plt.show()
+    plt.figure(figsize=(6,6))
+    plt.title('Mean Values')
+    plt.plot(r_med, label='<r>')
+    plt.plot(v_med, label='<v>')
+    plt.legend()
     plt.show()
